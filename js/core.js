234d3514ebca00e68c80f54ee258dbdea2347347
athimannil <athimannil@hotmail.com>
2014-08-07 01:30:02 +0100

    vanmaram application

---
+++ js/core.js
@@ -0,0 +1,4 @@
+var homePage = document.getElementById('home-form');
+// alert(window.outerHeight);
+// alert(homePage.offsetHeight);
+homePage.style.marginTop = ((window.innerHeight/2) - (homePage.offsetHeight/2)) +"px";
