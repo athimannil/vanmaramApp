3a86a330cdf91a0e9e14a96c25f04669b50dd18f
athimannil <athimannil@hotmail.com>
2014-08-13 23:27:59 +0100

    simplified to one page

--- js/core.js
+++ js/core.js
@@ -1,50 +1,9 @@
 var searchButton = document.getElementById("search");
 var searchWord = document.getElementById('search_word');
 var resultUl = document.getElementById("result");
+var listResult = document.getElementById("list-result");
 
-window.addEventListener('push', function(){
-    // alert("yes boss");
-    // console.log(wordFromLink);
-    // if (wordFromLink){
-    //     theWord = wordFromLink;
-    // }else{
-        theWord = localStorage.getItem("selectedWord");
-        console.log("idanu thirayendad  = " + theWord);
-        // theWord = theWord;
-    // }
-    resultUl.style.display = 'none';
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function(){
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
-            resultUl.style.display = 'block';
-            var decodedResp = JSON.parse(xmlhttp.responseText);
-            var resultLi = '';
-            for (i=0; i<decodedResp.length; i++){
-                resultLi += "<li class='table-view-cell'>" + decodedResp[i] + "</li>";
-            }
-            resultUl.innerHTML = resultLi;
-        }
-    };
-    xmlhttp.open("GET","http://www.vanmaram.com/json_result.php?en="+theWord,true);
-    xmlhttp.send();
-    // return false;
-});
-
-function submitHandler(wordFromLink){
-    // Check browser support
-    if (typeof(Storage) != "undefined") {
-        // Store
-        localStorage.setItem("selectedWord", wordFromLink);
-        // Retrieve
-        alert("set this value " + wordFromLink);
-        //console.log(localStorage.getItem("selectedWord"));
-        // document.getElementById("result").innerHTML = localStorage.getItem("lastname");
-    } else {
-        alert("the browser doesn\'t support storage");
-    }
-    // return false;
-}
-
+// auto suggest
 searchWord.onkeyup = function(e){
     if(e.keyCode == 13){
         return;
@@ -58,7 +17,7 @@ searchWord.onkeyup = function(e){
                 var decodedResp = JSON.parse(xmlhttp.responseText);
                 var resultLi = '';
                 for (i=0; i<decodedResp.length; i++){
-                    resultLi += "<li class='table-view-cell'><a href='result/answer.html' class='navigate-right suggested-word' data-transition='slide-in' onClick='return submitHandler(\""+decodedResp[i]+"\");' data-search-word='"+decodedResp[i]+"'>"+decodedResp[i]+"</a></li>";
+                    resultLi += "<li class='table-view-cell'><a class='navigate-right suggested-word' data-transition='slide-in' onClick='return submitHandler(\""+decodedResp[i]+"\");' data-search-word='"+decodedResp[i]+"'>"+decodedResp[i]+"</a></li>";
                 }
                 resultUl.innerHTML = resultLi;
             }
@@ -70,18 +29,29 @@ searchWord.onkeyup = function(e){
     }
 };
 
+// search result
+var submitHandler = function(wordFromLink) {
+    if (wordFromLink){
+        theWord = wordFromLink;
+    }else{
+        theWord = searchWord.value;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    resultUl.style.display = 'none';
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            // loadingSign.style.display = 'none';
+            resultUl.style.display = 'block';
+            var decodedResp = JSON.parse(xmlhttp.responseText);
+            var resultLi = '';
+            for (i=0; i<decodedResp.length; i++){
+                resultLi += "<li class='table-view-cell'>" + decodedResp[i] + "</li>";
+            }
+            resultUl.innerHTML = resultLi;
+        }
+    };
+    xmlhttp.open("GET","http://www.vanmaram.com/json_result.php?en="+theWord,true);
+    xmlhttp.send();
+    return false;
+};
