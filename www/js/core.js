6d597387f78429944d40f3ed5eb02b762c5b3f15
Muhammed Athimannil <athimannil@hotmail.com>
2014-09-17 14:22:41 +0100

    local storage
    pass parameter for local storage

--- www/js/core.js
+++ www/js/core.js
@@ -17,7 +17,9 @@ searchWord.onkeyup = function(e){
                 var decodedResp = JSON.parse(xmlhttp.responseText);
                 var resultLi = '';
                 for (i=0; i<decodedResp.length; i++){
-                    resultLi += "<li class='table-view-cell'><a class='navigate-right suggested-word' data-transition='slide-in' onClick='return submitHandler(\""+decodedResp[i]+"\");' data-search-word='"+decodedResp[i]+"'>"+decodedResp[i]+"</a></li>";
+                    // resultLi += "<li class='table-view-cell'><a class='navigate-right suggested-word' data-transition='slide-in' onClick='return submitHandler(\""+decodedResp[i]+"\");' data-search-word='"+decodedResp[i]+"'>"+decodedResp[i]+"</a></li>";
+                    // <a class="navigate-right" href="inbox.html" data-transition="slide-in" ontouchstart="logAlert('randamathedu');">
+                    resultLi += "<li class='table-view-cell'><a href='result/answer.html' class='navigate-right suggested-word' data-transition='slide-in' ontouchstart='getSearchValue(\""+decodedResp[i]+"\");' data-search-word='"+decodedResp[i]+"'>"+decodedResp[i]+"</a></li>";
                 }
                 resultUl.innerHTML = resultLi;
             }
@@ -28,7 +30,21 @@ searchWord.onkeyup = function(e){
         resultUl.style.display = 'none';
     }
 };
-
+function getSearchValue (selectedWord) {
+    if (typeof (localStorage) == 'undefined') {
+        console.log('Your browser does not support HTML5 localStorage.Try upgrading.');
+    } else {
+        try {
+            localStorage.setItem("name", selectedWord); //saves to the database, "key", "value"
+        } catch (e) {
+            if (e == QUOTA_EXCEEDED_ERR) {
+                alert('Quota exceeded!'); //data wasn't successfully saved due to quota exceed so throw an error
+            }
+        }
+        //document.write(localStorage.getItem("name")); //Hello World!
+        localStorage.removeItem("name"); //deletes the matching item from the database
+    }
+}
 // search result
 var submitHandler = function(wordFromLink) {
     if (wordFromLink){
